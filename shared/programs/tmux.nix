b678277177fc36fc9e1ab24fcafe88a7c50cbf48
pitaya <runningryuya@gmail.com>
2025-06-30 00:17:44 +0000

    Manage tmux plugins through nix.

--- shared/programs/tmux.nix
+++ shared/programs/tmux.nix
@@ -1,7 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [ tmux ];
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs.tmuxPlugins; [
+      resurrect
+      continuum
+    ];
+  };
   
   # Create the main tmux configuration file
   home.file.".tmux.conf".source = ./tmux/tmux.conf;
