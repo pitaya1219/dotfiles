10c59bb017d4c55434aeb65e79ae88b75a60c161
lepetitprince <ryuiti.yutori.labo@gmail.com>
2025-10-22 00:11:14 +0000

    chore: Rename user - lepetitweb to rose.

--- profiles/rose.nix
+++ profiles/rose.nix
@@ -16,13 +16,13 @@
           ../shared/programs/tmux.nix
           ../shared/programs/starship.nix
           ../shared/programs/readline.nix
-          ((import ../lib/neovim-extension.nix { inherit lib; }).forProfile "lepetitweb")
+          ((import ../lib/neovim-extension.nix { inherit lib; }).forProfile "rose")
           (import ../shared/programs/unfree.nix { additionalPackages = []; })
         ];
 
         home = {
-          username = "lepetitweb";
-          homeDirectory = "/home/lepetitweb";
+          username = "rose";
+          homeDirectory = "/home/rose";
           stateVersion = "23.11";
           packages = with pkgs; [];
         };
