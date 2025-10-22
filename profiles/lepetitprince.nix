82ab20756635bf4275f47f5d4afdac20530b2cdb
lepetitprince <ryuiti.yutori.labo@gmail.com>
2025-10-22 13:38:41 +0000

    chore: Change prompt color for lepetitprince.

--- profiles/lepetitprince.nix
+++ profiles/lepetitprince.nix
@@ -18,6 +18,7 @@
           ../shared/programs/starship.nix
           ../shared/programs/readline.nix
           ((import ../lib/neovim-extension.nix { inherit lib; }).forProfile "lepetitprince")
+          ((import ../lib/starship-extension.nix { inherit lib pkgs; }).forProfile "lepetitprince")
           (import ../shared/programs/unfree.nix { additionalPackages = []; })
         ];
 
