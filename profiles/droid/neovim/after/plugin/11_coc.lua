293c9ad6f67e707e2a2860dafb47cce3c7b78093
pitaya1219 <runningryuya@gmail.com>
2025-11-05 12:00:47 +0000

    chore: Add neovim plugins for droid profiles.

---
+++ profiles/droid/neovim/after/plugin/11_coc.lua
@@ -0,0 +1,10 @@
+-- CoC.nvim Configuration
+-- Merge existing global extensions with additional ones
+local existing_extensions = vim.g.coc_global_extensions or {}
+local addtional_extensions = {
+  '@yaegassy/coc-ruff',
+  '@yaegassy/coc-mypy',
+  'coc-pyright',
+  'coc-elixir',
+}
+vim.g.coc_global_extensions = vim.list_extend(existing_extensions, addtional_extensions)
