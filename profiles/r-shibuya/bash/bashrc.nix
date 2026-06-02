757766d0af6ebab1f66d79128f6b489136935c51
pitaya1219 <r-shibuya@tokyo-gas.co.jp>
2026-06-02 15:47:04 +0900

    refactor: Move ASANA_CLIENT_SECRET to bashrc, remove jq injection.

--- profiles/r-shibuya/bash/bashrc.nix
+++ profiles/r-shibuya/bash/bashrc.nix
@@ -1,3 +1,4 @@
 ''
 export PATH="~/.nix-profile/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$(brew --prefix openssl)/bin:/applications/xcode.app/contents/developer/usr/bin:$PATH"
+export ASANA_CLIENT_SECRET="$(passage show asana/client/secret 2>/dev/null)"
 ''
